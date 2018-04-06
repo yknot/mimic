86e911d5f3611c193b9f04e8b1da39133a5c490f
Andrew Yale <a.yale9@gmail.com>
2018-04-06 13:49:13 -0400

    add writing for decoding later

--- convert_mimic_SDV.py
+++ convert_mimic_SDV.py
@@ -1,3 +1,4 @@
+import pickle
 import pandas as pd
 from scipy.stats import truncnorm
 
@@ -19,37 +20,41 @@ def categorical(col):
         limits[b] = cat
         a = b
 
-    # create decoder function
-    def decode(x):
-        for k, v in limits.items():
-            if x < k:
-                return v
-
     # sample from the distributions and return that value
-    return col.apply(lambda x: distributions[x].rvs()), decode
+    return col.apply(lambda x: distributions[x].rvs()), limits
 
 
 def numeric(col):
     """normalize a numeric column"""
     return ((col - min(col)) / (max(col) - min(col)))
 
+# create decoder function
+
+
+def decode(x, limits):
+    for k, v in limits.items():
+        if x < k:
+            return v
+
 
 # read in data with column names
 df = pd.read_csv('data/final_df.csv')
 df = df.dropna()
+df = df.drop(['ADMITTIME', 'DISCHTIME'], axis=1)
 
 # test
-new_col, decode = categorical(df.ICU)
-assert ((new_col.apply(decode) == df.ICU).all())
+new_col, lim = categorical(df.ICU)
+assert ((new_col.apply(lambda x: decode(x, lim)) == df.ICU).all())
 
-decoders = {}
+limits = {}
 for c in df.columns:
     if df[c].dtype.char == 'O' or df[c].dtype.char == 'l':
-        new_col, decode = categorical(df[c])
+        new_col, lim = categorical(df[c])
         df[c] = new_col
-        decoders[c] = decode
+        limits[c] = lim
     elif df[c].dtype.char == 'd':
         df[c] = numeric(df[c])
 
-
+# save data and decoders
 df.to_csv('data/final_df_sdv.csv', index=False)
+pickle.dump(limits, open('data/decoders', 'wb'))
